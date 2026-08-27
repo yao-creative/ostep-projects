b26d3b8f7d7bca0252f54efcb890593cac0f0e91
lema-founders <yytanwork@gmail.com>
2026-08-27 12:13:54 +0800

    used exit from std lib

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 // No, you do not need to import anything extra to use stdout for writing
 // in C. Including <stdio.h> is sufficient, since it declares 'stdout'
 // as well as the fputs() function.
@@ -7,7 +9,7 @@
 int main(int argc, char *argv[]) {
     // Check if at least one filename is provided
     if (argc < 2) {
-        return 0;
+        exit(0);
     }
     // We'll process each file argument, one by one
     for (int i = 1; i < argc; i++) {
@@ -15,7 +17,7 @@ int main(int argc, char *argv[]) {
         FILE *fp = fopen(file, "r");
         if (fp == NULL) {
             printf("wcat: cannot open file\n");
-            return 1;
+            exit(1);
         }
         char buffer[4096];
         while (fgets(buffer, sizeof(buffer), fp) != NULL) {
@@ -23,5 +25,5 @@ int main(int argc, char *argv[]) {
         }
         fclose(fp);
     }
-    return 0;
+    exit(0);
 }
